9e382404a51bb6806ef7737b9b7734ad338f6e75
samatov-rsk <samativrsk@gmail.com>
2023-07-01 22:39:05 +0500

    after review

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,15 +7,15 @@ public class App {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
         Cat catBean = (Cat) applicationContext.getBean("cat");
-        Cat catBen2 = (Cat)applicationContext.getBean("cat");
+        Cat catBen2 = (Cat) applicationContext.getBean("cat");
 
-        HelloWorld helloBean = (HelloWorld)applicationContext.getBean("helloworld");
+        HelloWorld helloBean = (HelloWorld) applicationContext.getBean("helloworld");
         HelloWorld helloBean2 = (HelloWorld) applicationContext.getBean("helloworld");
 
         System.out.println(helloBean.getMessage());
         System.out.println(helloBean2.getMessage());
-        System.out.println(helloBean==helloBean2);
+        System.out.println(helloBean == helloBean2);
 
-        System.out.println(catBean==catBen2);
+        System.out.println(catBean == catBen2);
     }
 }
